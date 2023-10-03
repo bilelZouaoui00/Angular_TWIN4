dd6ad9f23a37a219a7f7866d45b711abfb4a7438
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2023-10-03 05:04:10 +0100

    Ex2 : Apply specific styles based on conditions in Ex2

--- src/app/fournisseur/fournisseur.component.ts
+++ src/app/fournisseur/fournisseur.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Fournisseur } from 'src/model/fournisseur';
 
 @Component({
   selector: 'app-fournisseur',
@@ -10,12 +11,29 @@ export class FournisseurComponent implements OnInit {
   code: string;
   libelle: string;
   hidden: boolean = false;
+  ListFournisseur : Fournisseur[];
 
   constructor() {
     this.idFournisseur = '123';
     this.code = 'ABC';
     this.libelle = 'Fournisseur XYZ';
+    this.ListFournisseur =[
+      {idFournisseur:1,code:"1ABC4522",libelle:"hp"},
+      {idFournisseur:2,code:"2ABC4522",libelle:"dell"},
+      {idFournisseur:3,code:"3ABC4522",libelle:"lenovo"},
+      {idFournisseur:400,code:"40ABC452",libelle:"asus"}
+      ];
+
+
   }
+
+  removeFournisseur(fournisseur: Fournisseur) {
+    const index = this.ListFournisseur.indexOf(fournisseur);
+    if (index !== -1) {
+      this.ListFournisseur.splice(index, 1);
+    }
+  }
+
 
 
   ngOnInit(): void {
