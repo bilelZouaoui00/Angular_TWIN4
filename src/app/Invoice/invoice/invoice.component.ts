5080c641a3db48076fd6f410533d5e60df0c13c6
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2023-10-04 22:43:16 +0100

    feat: work with queryParams & params

--- src/app/Invoice/invoice/invoice.component.ts
+++ src/app/Invoice/invoice/invoice.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-invoice',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./invoice.component.css']
 })
 export class InvoiceComponent {
+  invoiceId: number | undefined;
+  invoiceActive: boolean | undefined;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.invoiceId = +params['id'];
+     if (params['active'] != undefined)
+      this.invoiceActive = params['active'] === 'true';
+
+      console.log("invoice Id & Status : \n", this.invoiceId," & ",this.invoiceActive);
+    });
+    if (this.invoiceActive === undefined)
+    this.route.params.subscribe(params => {
+      this.invoiceId = +params['id'];
+     if (params['active'] != undefined)
+      this.invoiceActive = params['active'] === 'true';
+
+      console.log("invoice Id & Status : \n", this.invoiceId," & ",this.invoiceActive);
+    });
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { FournisseurComponent } from './fournisseur/fournisseur.component';
+import { MainInvoiceComponent } from './Invoice/main-invoice/main-invoice.component';
+import { InvoiceListComponent } from './Invoice/invoice-list/invoice-list.component';
+import { InvoiceComponent } from './Invoice/invoice/invoice.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'f', redirectTo: '/f', pathMatch: 'full' },
   { path: 'home', component: UserComponent },
-  { path: 'f', component: FournisseurComponent },
+  { path: 'fournisseur', component: FournisseurComponent },
+  // { path: 'invoice', component: MainInvoiceComponent },
+  { path: 'invoiceList', component: InvoiceListComponent },
+  { path: 'invoice/:id/:active', component: InvoiceComponent },
+  { path: 'invoice', component: InvoiceComponent },//is for ?param
+
 
 ];
 
