9a72907c94ff3c2fbdd7ddfc7662f1aee807514e
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2023-10-10 10:51:19 +0100

    feat: add routerLink connection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { FournisseurComponent } from './fournisseur/fournisseur.component';
+import { ListProduitsComponent } from './list-produits/list-produits.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'f', redirectTo: '/f', pathMatch: 'full' },
   { path: 'home', component: UserComponent },
   { path: 'f', component: FournisseurComponent },
+  { path: 'produits', component: ListProduitsComponent },
 
 ];
 
