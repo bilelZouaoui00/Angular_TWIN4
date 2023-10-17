37fbe9d7f603f965cd8f1a2525106270799aa6c8
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2023-10-17 10:19:40 +0100

    feat: add user model

---
+++ src/model/user.model.ts
@@ -0,0 +1,11 @@
+export interface User {
+  idCustomer: number;
+  firstName: string;
+  lastName: string;
+  birthDate: Date;
+  accountCategory: string;
+  email: string;
+  password: string;
+  picture: string;
+  profession: string;
+}
