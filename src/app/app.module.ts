e14f5765d5da0fc57160b274125d0b516bfe9ad5
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2023-11-11 21:46:27 +0100

    fix: update navigation links in app-navbar to use (click) events for consistent behavior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { NavbarComponent } from './navbar/navbar.component';
-
 import { FormsModule } from '@angular/forms';
 import { FournisseurComponent } from './fournisseur/fournisseur.component';
 import { ListFournisseurComponent } from './list-fournisseur/list-fournisseur.component';
 import { MainInvoiceComponent } from './Invoice/main-invoice/main-invoice.component';
 import { InvoiceListComponent } from './Invoice/invoice-list/invoice-list.component';
 import { InvoiceComponent } from './Invoice/invoice/invoice.component';
+import { SharedModule } from './shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    NavbarComponent,
     FournisseurComponent,
     ListFournisseurComponent,
     MainInvoiceComponent,
@@ -27,6 +23,7 @@ import { InvoiceComponent } from './Invoice/invoice/invoice.component';
     InvoiceComponent
   ],
   imports: [
+    SharedModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [UserComponent, AddUserComponent],
-  imports: [UserRoutingModule],
+  imports: [SharedModule,UserRoutingModule],
 })
 export class UserModule { }
